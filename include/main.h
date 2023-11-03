a51032d063236fd4178557034d45bc0d96858e27
HarryYCChou <harryyc.chou@gmail.com>
2023-11-02 17:21:31 -0700

    Fix all coding convension of main.h

--- include/main.h
+++ include/main.h
@@ -2,18 +2,21 @@
  * Copyright (c) 2023 Harry Chou
  */
 
-#ifndef INCLUDE_MAIN_HPP_
-#define INCLUDE_MAIN_HPP_
+#ifndef INCLUDE_MAIN_H_
+#define INCLUDE_MAIN_H_
 
-#include <iostream>
-#include <cstring>
-// imgui
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+// ==  c system headers  ==
 // glfw
 #include <GLFW/glfw3.h>
+// == c++ system headers ==
+#include <cstring>
+#include <iostream>
+// ==   other headers    ==
 // server
-#include "server.h"
+#include "./server.h"
+// imgui
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
-#endif  // INCLUDE_MAIN_HPP_
+#endif  // INCLUDE_MAIN_H_
--- include/server.h
+++ include/server.h
@@ -12,9 +12,9 @@
 #include <netinet/in.h>
 
 // imgui
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 // port
 #define PORT 62453
